97399b7bf5ebd09e1e48285b23a25f657330907b
Saiyantist <angelo.delossantos000@gmail.com>
2025-08-29 17:19:45 +0800

    Installed Bootstrap and Constructed initial structure of single page website
    - Learning Angular components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { Home } from './components/home/home';
+import { Gallery } from './components/gallery/gallery';
+import { Projects } from './components/projects/projects';
+import { Contact } from './components/contact/contact';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Home, Gallery, Projects, Contact, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
